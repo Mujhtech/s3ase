975a0700035b12c12bdc8382bec7ee11baa7d752
Muhideen Mujeeb Adeoye <mujeeb.muhideen@gmail.com>
2024-09-21 11:01:57 +0100

    init backend and frontend

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/mujhtech/s3ase
+
+go 1.23.0
---
+++ backend/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	s := "gopher"
+	fmt.Println("Hello and welcome, %s!", s)
+
+	for i := 1; i <= 5; i++ {
+		fmt.Println("i =", 100/i)
+	}
+}
