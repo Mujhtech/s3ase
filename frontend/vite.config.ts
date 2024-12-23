eb55420d6c15931101ffc988d25266040172f83f
Muhideen Mujeeb Adeoye <mujeeb.muhideen@gmail.com>
2024-12-23 10:31:40 +0100

    - update tailwind config - added packages - fix backend publish workflow

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -13,4 +14,5 @@ export default defineConfig({
     }),
     tsconfigPaths(),
   ],
+  test: {},
 });
