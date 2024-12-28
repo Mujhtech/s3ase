7f45b0d7816415f2df9c1a43f786a39a28a22020
Muhideen Mujeeb Adeoye <mujeeb.muhideen@gmail.com>
2024-12-28 09:00:57 +0100

    feat(file-upload): rename createFile to uploadFile and update request handling for multipart/form-data

--- frontend/app/services/file.server.ts
+++ frontend/app/services/file.server.ts
@@ -28,12 +28,31 @@ export async function getFile(request: Request, id: string) {
   return res.data;
 }
 
-export async function createFile(request: Request, body: CreateFileForm) {
+export async function uploadFile(
+  request: Request,
+  // file: File,
+  body?: CreateFileForm
+) {
+  // const formData = new FormData();
+  // formData.append("file", file);
+
+  // // Append other form data
+  // if (body) {
+  //   Object.entries(body).forEach(([key, value]) => {
+  //     if (value !== undefined) {
+  //       formData.append(key, value.toString());
+  //     }
+  //   });
+  // }
+
   const res = await api.post({
     request,
     path: "/ui/files",
     body: body,
     schema: GetFileSchema,
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
   });
 
   return res.data;
